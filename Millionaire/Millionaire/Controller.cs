5baeeb8e9567607f36ec27845609ff804f0c0d53
sebastian <sebs93@live.se>
2016-10-11 13:02:22 +0200

    controller added

---
+++ Millionaire/Millionaire/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Millionaire.DAL;
+
+namespace Millionaire
+{
+    
+    class Controller
+    {
+       DAL1 dal = new DAL1();
+
+        private bool createUser(string userName, string password, string userType)
+        {
+            return dal.CreateUser(userName, password, userType);
+        }
+        private void updateUser(string userName, string password, string userType)
+        {
+        }
+        private void deleteUser(string userName, string password, string userType)
+        {
+        }
+    }
+}
