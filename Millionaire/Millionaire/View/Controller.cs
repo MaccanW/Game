d3e4536edbc3bfff7b4ad823867cdd0772b76dc1
Marcus <Markan_3@hotmail.com>
2016-10-21 11:54:38 +0200

    controlelr

--- Millionaire/Millionaire/View/Controller.cs
+++ Millionaire/Millionaire/View/Controller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Millionaire.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace Millionaire.View
 {
     class Controller
     {
+        DAL1 dal = new DAL1();
+
+        private bool createUser(string userName, string password, string userType)
+        {
+            return dal.CreateUser(userName, password, userType);
+        }
+        private void updateUser(string userName, string password, string userType)
+        {
+        }
+        private void deleteUser(string userName, string password, string userType)
+        {
+        }
     }
 }
