3d07c3c0e306e568084422b923968789774adea4
Marcus <Markan_3@hotmail.com>
2016-10-21 12:52:20 +0200

    DAL

--- Millionaire/Millionaire/DAL/DAL1.cs
+++ Millionaire/Millionaire/DAL/DAL1.cs
@@ -65,15 +65,46 @@ namespace Millionaire.DAL
             catch
             {
                 Connect().Close();
-                
+          
             }
+        }
+        //Create or update question
+        private bool CreateOrUpdateQuestion(int id, string question, string rightAnswer, int level, string category, string creator, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3, string sqlCommand)
+        {
+            string sql = "execute usp_createQuestion" + @id + ", '" + @question + "', ' " + @rightAnswer + "'" + @level + ", '" + @category + "', " + @creator + "', '" + @wrongAnswer1 + "', '" + @wrongAnswer2 + "', '" + @wrongAnswer3 + "', '" + @wrongAnswer3 + "', '" + @sqlCommand + "'";
+
+            SqlCommand cmd = new SqlCommand(sql, Connect());
+
+            cmd.Parameters.Add(new SqlParameter("id", id));
+            cmd.Parameters.Add(new SqlParameter("question", question));
+            cmd.Parameters.Add(new SqlParameter("rightAnswer", rightAnswer));
+            cmd.Parameters.Add(new SqlParameter("level", level));
+            cmd.Parameters.Add(new SqlParameter("category", category));
+            cmd.Parameters.Add(new SqlParameter("creator", creator));
+            cmd.Parameters.Add(new SqlParameter("wrongAnswer1", wrongAnswer1));
+            cmd.Parameters.Add(new SqlParameter("wrongAnswer2", wrongAnswer1));
+            cmd.Parameters.Add(new SqlParameter("wrongAnswer3", wrongAnswer1));
+            cmd.Parameters.Add(new SqlParameter("sqlCommand", sqlCommand));
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                Connect().Close();
+                return true;
+            }
+            catch
+            {
+                Connect().Close();
+                return false;
+            }
+
         }
 
 
 
 
 
- 
+
 
 
 
